b06234a2a4d0c19abac51c33876a2cd5831c0249
kazunori sasaki <kazyury@gmail.com>
2013-02-03 23:09:32 +0900

    implemented scene changing.

---
+++ MementoWeaver/src/nobugs/nolife/mw/ui/AppMain.java
@@ -0,0 +1,76 @@
+package nobugs.nolife.mw.ui;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class AppMain extends Application {
+	private Stage stage;
+
+	// お約束
+	public static void main(String[] args) { launch(args); }
+
+	@Override
+	public void start(Stage primaryStage) {
+		stage = primaryStage;
+		stage.setTitle("Memento Weaver");  // 画面のタイトルを設定
+		fwdMainMenu();
+		stage.show(); // 画面表示
+	}
+
+	/**
+	 * 画面遷移メソッド(MainMenuへの遷移)
+	 */
+	void fwdMainMenu() {
+		try {
+			MainMenuController next = (MainMenuController) replaceSceneContent("MainMenu.fxml");
+			next.setApplication(this);
+		} catch (Exception ex) {
+			// TODO error handling
+		}
+	}
+
+	/**
+	 * 画面遷移メソッド(InstallMaterialへの遷移)
+	 */
+	void fwdInstallMaterial() {
+		try {
+			InstallMaterialController next = (InstallMaterialController) replaceSceneContent("InstallMaterial.fxml");
+			next.setApplication(this);
+		} catch (Exception ex) {
+			// TODO error handling
+		}
+	}
+
+	/**
+	 * 
+	 * @param fxml
+	 * @return fxmlで指定されたシーンに対応したコントローラをInitializableとして返却
+	 * @throws IOException 
+	 */
+	private Initializable replaceSceneContent(String fxml) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		InputStream in = AppMain.class.getResourceAsStream(fxml);
+		loader.setBuilderFactory(new JavaFXBuilderFactory());
+		loader.setLocation(AppMain.class.getResource(fxml));
+		AnchorPane page;
+
+		try {
+			page = (AnchorPane) loader.load(in);
+		} finally {
+			in.close();
+		}
+		Scene scene = new Scene(page, 800, 600);
+		stage.setScene(scene);
+		stage.sizeToScene();
+		return (Initializable) loader.getController();
+	}
+}
--- MementoWeaver/src/nobugs/nolife/mw/ui/InstallMaterialController.java
+++ MementoWeaver/src/nobugs/nolife/mw/ui/InstallMaterialController.java
@@ -6,22 +6,23 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class MainMenuController extends AnchorPane implements Initializable {
+public class InstallMaterialController extends AnchorPane implements Initializable{
 	private AppMain appl;
 	
 	// 入力フィールドに対応するインスタンスを保持する変数
 	// 対応付けはFXMLファイルで定義する
 	// publicフィールドの場合は@FXMLの記述は不要
+	@FXML private TextField pathInput;
 
+	
 	// イベントハンドラ
 	@FXML	protected void install(ActionEvent e) {
 		System.out.println(e);
-		appl.fwdInstallMaterial();
 	}
-	@FXML	protected void generate(ActionEvent e) {} // TODO not implemented yet
-	@FXML	protected void modify(ActionEvent e) {} // TODO not implemented yet
+	@FXML	protected void browse(ActionEvent e) {} // TODO not implemented yet
 	@FXML	protected void exit(ActionEvent e) {} // TODO not implemented yet
 
 	@Override
@@ -32,4 +33,5 @@ public class MainMenuController extends AnchorPane implements Initializable {
 	public void setApplication(AppMain appMain) {
 		appl = appMain;
 	}
+
 }
