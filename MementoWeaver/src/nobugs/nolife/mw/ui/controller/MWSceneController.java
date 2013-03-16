43134ace9dba590c099b2866c2fdf0f4c4f4575c
kazunori sasaki <kazyury@gmail.com>
2013-03-17 00:33:25 +0900

    余計なコメント除去

--- MementoWeaver/src/nobugs/nolife/mw/ui/controller/MWSceneController.java
+++ MementoWeaver/src/nobugs/nolife/mw/ui/controller/MWSceneController.java
@@ -2,7 +2,7 @@ package nobugs.nolife.mw.ui.controller;
 
 import nobugs.nolife.mw.AppMain;
 import javafx.fxml.Initializable;
-// TODO ‰æ–ÊControllerŒn‚ÆBusiness ProcessingŒn‚ð•ª—£
+// ‰æ–ÊControllerŒn‚ÆBusiness ProcessingŒn‚ð•ª—£
 public interface MWSceneController extends Initializable {
 	public void setApplication(AppMain appMain, Object bulk);
 }
