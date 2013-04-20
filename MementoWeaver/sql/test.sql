6297983e7b0895d1143172e0f2efef61bbd61919
kazunori sasaki <kazyury@gmail.com>
2013-04-20 11:54:29 +0900

    added some sqls for testing.

--- MementoWeaver/sql/test.sql
+++ MementoWeaver/sql/test.sql
@@ -9,3 +9,15 @@ delete from MW.MEMENTO;
 delete from mw.material;
 
 select * from MW.PREDEFINED_TAG;
+
+
+delete from mw.memento_contents where material_id='20130203063324' and tag='album';
+delete from mw.tagged_material where material_id='20130203063324' and tag='album';
+
+insert into MW.MEMENTO_CONTENTS values('20130103085623','album','a_201301');
+insert into MW.MEMENTO_CONTENTS values('20130203063324','hiroko','hiroko');
+insert into MW.MEMENTO_CONTENTS values('20130203063324','party','p_2013');  
+insert into MW.MEMENTO_CONTENTS values('20130203063416','treasure','t_age10'); 
+insert into MW.MEMENTO_CONTENTS values('20130203063416','winner','w_201302');
+
+
