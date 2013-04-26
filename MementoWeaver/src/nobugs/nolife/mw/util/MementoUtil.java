9af9d31b86fd05c870ffe6eadbd7853aec7478c0
kazunori sasaki <kazyury@gmail.com>
2013-04-26 22:37:32 +0900

    changed to use Constants

--- MementoWeaver/src/nobugs/nolife/mw/util/MementoUtil.java
+++ MementoWeaver/src/nobugs/nolife/mw/util/MementoUtil.java
@@ -13,7 +13,7 @@ public class MementoUtil {
 		String category = memento.getCategory();
 		String mementoId = memento.getMementoId();
 		
-		if("albumPage".equals(category)){
+		if(Constants.MEMENTO_CATEGORY_ALBUM.equals(category)){
 			// materialIdがmementoId(ex.a_201301)の年月部分から始まっていない場合はNG
 			if(materialId.startsWith(mementoId.substring(2))){
 				logger.info("albumPage["+mementoId+"]はMaterialID["+materialId+"]を追加可能です.");
@@ -23,11 +23,11 @@ public class MementoUtil {
 				ret=false;
 			}
 			
-		} else if("chronicle".equals(category)) {
+		} else if(Constants.MEMENTO_CATEGORY_CHRONICLE.equals(category)) {
 			logger.info("chronicle["+mementoId+"]はMaterialID["+materialId+"]を追加可能です.");
 			ret = true; // chronicleの場合はいつでもOK
 			
-		} else if("partyPage".equals(category)) {
+		} else if(Constants.MEMENTO_CATEGORY_PARTY.equals(category)) {
 			// materialIdがmementoId(ex.p_2013)の年部分から始まっていない場合はNG
 			if(materialId.startsWith(mementoId.substring(2))){
 				logger.info("partyPage["+mementoId+"]はMaterialID["+materialId+"]を追加可能です.");
@@ -37,7 +37,7 @@ public class MementoUtil {
 				ret=false;
 			}
 			
-		} else if("treasurePage".equals(category)) {
+		} else if(Constants.MEMENTO_CATEGORY_TREASURE.equals(category)) {
 			// materialId時点の年齢がmementoId(ex.t_age10)の年齢部分と一致しない場合NG
 			String age = AgeCalculator.calcAgeAsFormattedString("taito", materialId.substring(0, 9));
 			if(age.equals(mementoId.substring(5))){
@@ -48,7 +48,7 @@ public class MementoUtil {
 				ret=false;
 			}
 			
-		} else if("winnerPage".equals(category)) {
+		} else if(Constants.MEMENTO_CATEGORY_WINNER.equals(category)) {
 			// materialIdがmementoId(ex.w_201301)の年部分から始まっていない場合はNG
 			if(materialId.startsWith(mementoId.substring(2))){
 				logger.info("winnerPage["+mementoId+"]はMaterialID["+materialId+"]を追加可能です.");
